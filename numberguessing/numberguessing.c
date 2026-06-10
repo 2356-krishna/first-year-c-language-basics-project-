c031b2629ab930fab3c5e3f772c2cc3c8eeed0b8
Krishna_codes <krishnavohra251@gmail.com>
2026-06-11 01:19:24 +0530

    Add files via upload

--- numberguessing/numberguessing.c
+++ numberguessing/numberguessing.c
@@ -1,1 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
+int main(){
+    
+    
+    
+    srand(time(0));
+    int random = (rand()%100)+1;
+    int n;
+    int no_of_guesses;
+    printf("%d",random);
+    
+     for (no_of_guesses = 1; 2 < 3 ; no_of_guesses++)
+     {
+        printf("Guess random numbers between 1-100 = ");
+        scanf("%d",&n);
+        if(n==random)
+        {
+            printf("CONGRATS! ,YOU GUESS THE CORRECT NUMBER \n");
+            break;
+        }
+        else if(n<random)
+        {
+            printf("You guess the lowest try higher number to win \n");
+        }
+        else if(n>random)
+        {
+            printf("You guess the highest number try lowest number to win \n");
+        }
+     }
+    printf("You Guessed %d times to win",no_of_guesses);
+
+     
+     return 0;
+}
